d255eb247fce6035b9fec834fb5ba6c7e1fd6e1a
Курзаев Виктор <nisevitek@gmail.com>
2025-03-17 17:55:48 +0700

    задания готовы

--- variables/task-4/index.js
+++ variables/task-4/index.js
@@ -1,21 +1,21 @@
 let count = 0;
 let year = 2024;
 
-let userOne = prompt("Введите имя пользователя №1");
-let userOneYear = prompt("Введите год рождения №1");
+let userOne = prompt("Введите имя пользователя №1", "Антон");
+let userOneYear = prompt("Введите год рождения №1", 2003);
 let userOneAge = year - userOneYear
 
 console.log(++count, userOne, userOneAge);
 
-let userTwo = prompt("Введите имя пользователя №2");
-let userTwoYear = prompt("Введите год рождения №2");
-let userTwoAge = year - userOneYear
+let userTwo = prompt("Введите имя пользователя №2", "Василиса");
+let userTwoYear = prompt("Введите год рождения №2", 2005);
+let userTwoAge = year - userTwoYear
 
 console.log(++count, userTwo, userTwoAge);
 
-let userThree = prompt("Введите имя пользователя №3");
-let userThreeYear = prompt("Введите год рождения №3");
-let userThreeAge = year - userOneYear
+let userThree = prompt("Введите имя пользователя №3", "Никита");
+let userThreeYear = prompt("Введите год рождения №3", 1998);
+let userThreeAge = year - userThreeYear
 
 console.log(++count, userThree, userThreeAge);
 
